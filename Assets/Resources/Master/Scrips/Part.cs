1278e286db29b9cfbc5893c1e2e7e3cec26d9bf8
Kilian <pflugk@hotmail.de>
2020-02-21 01:32:26 +0100

    Added some Funcs /01:33
    -added code for part&car ui
    -func for buying part
    . logic of part changed
    ... (show comments)

--- Assets/Resources/Master/Scrips/Part.cs
+++ Assets/Resources/Master/Scrips/Part.cs
@@ -27,9 +27,9 @@ public class Part
 
 
 
-    public int requiredScrap;
-    public int requiredPlastics;
-    public int requiredElectronics;
+    public int requiredScrap = 0;
+    public int requiredPlastics = 0;
+    public int requiredElectronics = 0;
 
     public SortOfPart sort;
 
@@ -37,41 +37,69 @@ public class Part
 
     public Button button;
 
-    public Part(int _requiredScrap,
-                int _requiredPlastics,
-                int _requiredElectronics,
-                SortOfPart _sort,
+
+    //---------------KILIAN NEU--------------------------------------------------------------------------------//
+    public Part(SortOfPart _sort,
                 Button _button)
     {
-        this.requiredScrap = _requiredScrap;
-        this.requiredPlastics = _requiredPlastics;
-        this.requiredElectronics = _requiredElectronics;
+    //---------------KILIAN NEU--------------------------------------------------------------------------------//
         this.button = _button;
         this.sort = _sort;
 
 
         if (this.sort == SortOfPart.Engine)
         {
+            this.requiredScrap = 7;
+            this.requiredPlastics = 3;
+            this.requiredElectronics = 5;
+
             this.level = Variables.engineLevel;
-            Variables.carValueMultiplier = Variables.carValueMultiplier + (0.25f * this.level);
+            if (this.level >= 2)
+            {
+                this.requiredScrap = this.requiredScrap + (2 * this.level);
+                this.requiredPlastics = this.requiredPlastics + (2 * this.level);
+                this.requiredElectronics = this.requiredElectronics + (2 * this.level);
+                Variables.carValueMultiplier = Variables.carValueMultiplier + (0.25f * this.level);
+
+            }
         }
 
 
         if (this.sort == SortOfPart.Frame)
         {
+            this.requiredScrap = 10;
+            this.requiredPlastics = 3;
+            this.requiredElectronics = 2;
+
             this.level = Variables.frameLevel;
-            Variables.carValueMultiplier = Variables.carValueMultiplier + (0.25f * this.level);
+            if (this.level >= 2)
+            {
+                this.requiredScrap = this.requiredScrap + (2 * this.level);
+                this.requiredPlastics = this.requiredPlastics + (2 * this.level);
+                this.requiredElectronics = this.requiredElectronics + (2 * this.level);
+                Variables.carValueMultiplier = Variables.carValueMultiplier + (0.25f * this.level);
+            }
         }
 
 
         if (this.sort == SortOfPart.Tire)
         {
+            this.requiredScrap = 3;
+            this.requiredPlastics = 10;
+            this.requiredElectronics = 2;
+
             this.level = Variables.tireLevel;
-            Variables.carValueMultiplier = Variables.carValueMultiplier + (0.25f * this.level);
+            if (this.level >= 2)
+            {
+                this.requiredScrap = this.requiredScrap + (2 * this.level);
+                this.requiredPlastics = this.requiredPlastics + (2 * this.level);
+                this.requiredElectronics = this.requiredElectronics + (2 * this.level);
+                Variables.carValueMultiplier = Variables.carValueMultiplier + (0.25f * this.level);
+            }
         }
 
 
-        
+
 
 
 
